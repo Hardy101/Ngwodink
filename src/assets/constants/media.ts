ec368f2b99cd1e844cf74cd1bb5c6e933e4534be
Eghosa Ordia <eghordia130@gmail.com>
2025-05-09 21:06:02 +0100

    Add extensible media asset and update Customizable component: included new extensible image in media constants and adjusted layout and styling in the Customizable component for improved presentation and user engagement.

--- src/assets/constants/media.ts
+++ src/assets/constants/media.ts
@@ -1,9 +1,11 @@
 import logo from "../img/logo.png";
 import debug from "../img/debug.png";
 import git from "../img/git.png";
+import extensible from "../img/extensible.png"
 
 export const media = {
   logo,
   debug,
   git,
+  extensible
 };
--- src/components/extensible.tsx
+++ src/components/extensible.tsx
@@ -1,7 +1,9 @@
+import { media } from "../assets/constants/media";
+
 const Customizable: React.FC = () => {
   return (
     <section>
-      <div className="relative px-16 py-16 font-manrope bg-dark-secondary text-white overflow-clip">
+      <div className="relative grid grid-cols-2 px-16 py-16 font-manrope bg-dark-secondary text-white overflow-clip">
         <div
           className="absolute w-1/4 h-1/2 top-20 left-36 bg-green-primary"
           style={{
@@ -10,11 +12,11 @@ const Customizable: React.FC = () => {
           }}
         ></div>
         <div className="absolute w-full h-full top-0 left-0 bg-dark-secondary/20 backdrop-blur-3xl"></div>
-        <div className="relative text grid gap-y-6 z-10">
+        <div className="relative text grid gap-y-6 my-auto z-10">
           <h2 className="text-5xl font-manrope-bold">
             Extensible and customizable
           </h2>
-          <p className="w-2/5 text-lg">
+          <p className="w-4/5 text-lg">
             Want even more features? Install extensions to add new languages,
             themes, debuggers, and to connect to additional services
           </p>
@@ -22,6 +24,9 @@ const Customizable: React.FC = () => {
             <span>View Intellisense</span>
             <i className="fa-solid fa-chevron-right"></i>
           </button>
+        </div>
+        <div className="img relative z-10">
+          <img src={media.extensible} alt="image of programming languages" className="mx-auto"/>
         </div>
       </div>
     </section>
